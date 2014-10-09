54af97dd64c60c4df91e4d27693b4ca899a18379
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2014-10-08 19:12:19 -0700

    Clean project

--- authentication/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=16, required=True)
-    password = forms.CharField(widget=forms.PasswordInput())
