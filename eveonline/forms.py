eed40b39833f964c65445a5d3facc4f8e9f79583
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2014-10-12 13:32:46 -0700

    Added new labels for api key

--- eveonline/forms.py
+++ eveonline/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class UpdateKeyForm(forms.Form):
-    api_id = forms.CharField(max_length=254, required=True)
-    api_key = forms.CharField(max_length=254, required=True)
+    api_id = forms.CharField(max_length=254, required=True, label="Key ID")
+    api_key = forms.CharField(max_length=254, required=True, label="Verification Code")
