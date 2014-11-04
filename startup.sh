1f317ae7114299b230badc599e69030ad5bf397e
Trent Bartlem <trent.bartlem@gmail.com>
2014-11-04 11:28:49 +1000

    Extracted alliance corp update to new file

--- startup.sh
+++ startup.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 python manage.py syncdb
 
-python manage.py shell
-    from util import bootstrap_permissions
-    from celerytask.tasks import run_alliance_corp_update
-    bootstrap_permissions()
-    run_alliance_corp_update()
+python manage.py shell < run_alliance_corp_update.py
 
 python manage.py celeryd --verbosity=2 --loglevel=DEBUG
 python manage.py celerybeat --verbosity=2 --loglevel=DEBUG
