9b1413768152ecf4596c1ed104bc885a5478d5c3
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2014-11-04 16:44:27 -0700

    Fixed issue with adding api keys.

--- eveonline/forms.py
+++ eveonline/forms.py
@@ -7,13 +7,19 @@ from eveonline.managers import EveManager
 class UpdateKeyForm(forms.Form):
     api_id = forms.CharField(max_length=254, required=True, label="Key ID")
     api_key = forms.CharField(max_length=254, required=True, label="Verification Code")
-    is_blue = forms.BooleanField(label="Blue to alliance")
+    is_blue = forms.BooleanField(label="Blue to alliance", required=False)
 
     def clean(self):
         if EveManager.check_if_api_key_pair_exist(self.cleaned_data['api_id']):
             raise forms.ValidationError(u'API key already exist')
 
-        if not self.cleaned_data['is_blue']:
+        check_blue = False
+        try:
+            check_blue = self.cleaned_data['is_blue']
+        except:
+            pass
+
+        if not check_blue:
             if not EveApiManager.check_api_is_type_account(self.cleaned_data['api_id'],
                                                            self.cleaned_data['api_key']):
                 raise forms.ValidationError(u'API not of type account')
--- util/__init__.py
+++ util/__init__.py
@@ -7,6 +7,7 @@ from django.conf import settings
 
 def bootstrap_permissions():
     ct = ContentType.objects.get_for_model(User)
+    Permission.objects.get_or_create(codename="alliance_member", content_type=ct, name="alliance_member")
     Permission.objects.get_or_create(codename="group_management", content_type=ct, name="group_management")
     Permission.objects.get_or_create(codename="jabber_broadcast", content_type=ct, name="jabber_broadcast")
     Permission.objects.get_or_create(codename="human_resources", content_type=ct, name="human_resources")
@@ -19,22 +20,21 @@ def add_member_permission(user, permission):
     ct = ContentType.objects.get_for_model(User)
     stored_permission, created = Permission.objects.get_or_create(codename=permission,
                                                                   content_type=ct, name=permission)
-
-    if User.objects.filter(username=user.username).exists():
-        user = User.objects.get(username=user.username)
-        user.user_permissions.add(stored_permission)
-        user.save()
+    user = User.objects.get(username=user.username)
+    user.user_permissions.add(stored_permission)
+    user.save()
 
 
 def remove_member_permission(user, permission):
     ct = ContentType.objects.get_for_model(User)
     stored_permission, created = Permission.objects.get_or_create(codename=permission,
                                                                   content_type=ct, name=permission)
-    if User.objects.filter(username=user.username).exists():
-        user = User.objects.get(username=user.username)
-        if user.has_perm(permission):
-            user.user_permissions.remove(stored_permission)
-            user.save()
+
+    user = User.objects.get(username=user.username)
+
+    if user.has_perm('auth.' + permission):
+        user.user_permissions.remove(stored_permission)
+        user.save()
 
 
 def check_if_user_has_permission(user, permission):
