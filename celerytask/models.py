121cd5e9a65af9b5bb6e6bb19eca288a1ecaf42b
Raynaldo Rivera <royal.r4stl1n@gmail.com>
2014-11-22 20:39:54 -0700

    Added details to syncgroup cache

--- celerytask/models.py
+++ celerytask/models.py
@@ -5,3 +5,6 @@ from django.contrib.auth.models import User
 class SyncGroupCache(models.Model):
     groupname = models.CharField(max_length=254)
     user = models.ForeignKey(User)
+
+    def __str__(self):
+        return self.user.username + ' - ' + self.groupname + ' - SyncGroupCache'
