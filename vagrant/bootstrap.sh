97e2f1c09c2150fd0e5f161113719e27175f1ef9
Trent Bartlem <trent.bartlem+github@gmail.com>
2015-01-04 23:59:18 +1000

    updated to match new services

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+sudo add-apt-repository ppa:upubuntu-com/xampp
+
 sudo apt-get update
 
 sudo apt-get clean
 
 sudo apt-get -y install libtool
+sudo apt-get -y install git
 sudo apt-get -y install python-dev libyaml-dev libffi-dev
 sudo apt-get -y install python-pip
+sudo apt-get -y install mysql-server mysql-client
 
 cd /vagrant/
 
@@ -34,10 +38,14 @@ sudo mysqladmin status -p$MYSQL_ROOT_PASS
 echo 'Creating databases and allianceauth user'
 sudo mysqladmin -p$MYSQL_ROOT_PASS create alliance_auth 
 sudo mysqladmin -p$MYSQL_ROOT_PASS create alliance_forum
+sudo mysqladmin -p$MYSQL_ROOT_PASS create alliance_jabber
 sudo mysqladmin -p$MYSQL_ROOT_PASS create alliance_mumble
+sudo mysqladmin -p$MYSQL_ROOT_PASS create alliance_killboard
 
 sudo mysql -u root -p$MYSQL_ROOT_PASS -e "CREATE USER 'allianceauth'@'localhost' IDENTIFIED BY 'allianceauth'"
 sudo mysql -u root -p$MYSQL_ROOT_PASS -e "GRANT ALL PRIVILEGES ON * . * TO 'allianceauth'@'localhost'";
+sudo mysql -u root -p$MYSQL_ROOT_PASS -e "CREATE USER 'alliancemumble'@'localhost' IDENTIFIED BY 'alliancemumble'"
+sudo mysql -u root -p$MYSQL_ROOT_PASS -e "GRANT ALL PRIVILEGES ON * . * TO 'alliancemumble'@'localhost'";
 
 sudo mysqladmin -p$MYSQL_ROOT_PASS flush-privileges
 
@@ -46,6 +54,8 @@ sudo apt-get -y install rabbitmq-server
 sudo pip install -r requirements.txt
 
 chmod +x *.sh
+
+sudo apt-get -y install xampp
 
 echo '--------'
 echo 'Almost there!'
