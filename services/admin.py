2d85fb84ed8578c66d8ff4522744bdf030df0558
Brett Costabile <kaezontpa@gmail.com>
2015-05-23 17:00:56 -0400

    Added TS3 group models and admin

--- services/admin.py
+++ services/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+from .models import authTS
+from .models import TSgroup
+from .models import userTSgroup
+
+class AuthTSgroupAdmin(admin.ModelAdmin):
+	fields = ['ts_group_id','auth_group_id']
+
+class TSgroupAdmin(admin.ModelAdmin):
+	fields = ['name']
+
+admin.site.register(authTS, AuthTSgroupAdmin)
+admin.site.register(TSgroup, TSgroupAdmin)
