8a0e3f309db71562c7a9d53c9ccf6d471712e8a7
Brett Costabile <kaezontpa@gmail.com>
2015-11-02 00:41:31 -0500

    More logging
    Groups aren't adding now. Gotta make the logging better in
    update_groups()

--- services/managers/teamspeak3_manager.py
+++ services/managers/teamspeak3_manager.py
@@ -212,9 +212,9 @@ class Teamspeak3Manager:
 
     @staticmethod
     def update_groups(uid, ts_groups):
-        print "Running update_groups" 
-        print uid
-        print ts_groups
+        print "--Running update_groups--" 
+        print("uid = {0}").format(uid)
+        print("ts_groups = {0}").format(ts_groups)
         userid = Teamspeak3Manager._get_userid(uid)
         addgroups = []
         remgroups = []
@@ -224,12 +224,12 @@ class Teamspeak3Manager:
                 user_ts_groups[key] = int(user_ts_groups[key])
             print("user_ts_groups = {0}").format(user_ts_groups)
             for ts_group_key in ts_groups:
-                print("(For addgroups) GID {0}").format(ts_group_key)
+                print("(For addgroups) GID: {0} Name: {1}").format(ts_groups[ts_group_key],ts_group_key)
                 if ts_groups[ts_group_key] not in user_ts_groups.values():
                     print("Adding {0}").format(ts_group_key)
                     addgroups.append(ts_groups[ts_group_key])
             for user_ts_group_key in user_ts_groups:
-                print("(For remgroups) GID {0}").format(user_ts_group_key)
+                print("(For remgroups) GID: {0} Name: {1}").format(user_ts_groups[user_ts_group_key],user_ts_group_key)
                 if user_ts_groups[user_ts_group_key] not in ts_groups.values():
                     print("Value {0} not found").format(user_ts_groups[user_ts_group_key])
                     remgroups.append(user_ts_groups[user_ts_group_key])
