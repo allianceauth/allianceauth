1cc3b2a6b7f70e7657677e7dec44092a239bfc2c
Adarnof <adarnof@gmail.com>
2015-11-07 05:33:46 +0000

    Fixed Issue #77 Teamspeak3 Manger errors prevent run_databaseUpdate from completing Wrapped _sync_ts_group_db in a try/catch block until we abstract services into their own apps

--- services/managers/teamspeak3_manager.py
+++ services/managers/teamspeak3_manager.py
@@ -115,19 +115,22 @@ class Teamspeak3Manager:
 
     @staticmethod
     def _sync_ts_group_db():
-        remote_groups = Teamspeak3Manager._group_list()
-        local_groups = TSgroup.objects.all()
-        for key in remote_groups:
-            remote_groups[key] = int(remote_groups[key])
+        try:
+            remote_groups = Teamspeak3Manager._group_list()
+            local_groups = TSgroup.objects.all()
+            for key in remote_groups:
+                remote_groups[key] = int(remote_groups[key])
             
-        for group in local_groups:
-            if group.ts_group_id not in remote_groups.values():
-                TSgroup.objects.filter(ts_group_id=group.ts_group_id).delete()
-        for key in remote_groups:
-            g = TSgroup(ts_group_id=remote_groups[key],ts_group_name=key)
-            q = TSgroup.objects.filter(ts_group_id=g.ts_group_id)
-            if not q:
-                g.save()
+            for group in local_groups:
+                if group.ts_group_id not in remote_groups.values():
+                    TSgroup.objects.filter(ts_group_id=group.ts_group_id).delete()
+            for key in remote_groups:
+                g = TSgroup(ts_group_id=remote_groups[key],ts_group_name=key)
+                q = TSgroup.objects.filter(ts_group_id=g.ts_group_id)
+                if not q:
+                    g.save()
+        except:
+            pass
 
     @staticmethod
     def add_user(username, corp_ticker):
