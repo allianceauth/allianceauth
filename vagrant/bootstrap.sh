57a1c6aaaa035802b289ae91c6ba8165afa73dc2
Brett Costabile <kaezontpa@gmail.com>
2015-11-18 23:08:59 -0500

    Removed experimental setup
    Alot of my experimental stuff for postgresql was causing issues, so I'm
    removing that for a different project. This way I can get a clean setup
    out.

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -13,30 +13,6 @@ sudo apt-get -y install python-dev libyaml-dev libffi-dev
 sudo apt-get -y install python-pip
 sudo apt-get -y install mysql-server mysql-client libmysqlclient-dev
 
-#Python 3 stuff
-sudo apt-get -y install python-software-properties
-sudo add-apt-repository ppa:fkrull/deadsnakes
-sudo apt-get -y update
-sudo apt-get -y install python3.4
-
-#virtualenv stuff
-sudo apt-get -y install python-virtualenv
-sudo virtualenv -p /usr/bin/python3.4 /opt/allianceVenv
-
-#Postgresql stuff
-sudo apt-get -y postgresql-9.1 libpq5 libpq-dev postgresql-client-9.1 postgresql-common ssl-cert postgresql-contrib-9.1 postgresql-server-dev-9.1
-sudo apt-get -y install -f
-cat << EOF | su - postgres -c psql
--- Create database user:
-CREATE USER vagrant WITH PASSWORD 'password';
-GRANT ALL PRIVILEGES ON DATABASE alliance% to vagrant;
-EOF
-createdb alliance_auth
-createdb alliance_forum
-createdb alliance_jabber
-createdb alliance_mumble
-createdb alliance_killboard
-
 cd /vagrant/
 
 sudo pip install --upgrade pip
@@ -65,7 +41,7 @@ sudo mysqladmin -p$MYSQL_ROOT_PASS create alliance_jabber
 sudo mysqladmin -p$MYSQL_ROOT_PASS create alliance_mumble
 sudo mysqladmin -p$MYSQL_ROOT_PASS create alliance_killboard
 
-sudo mysql -u root -p$MYSQL_ROOT_PASS -e "CREATE USER 'allianceauth'@'localhost' IDENTIFIED BY 'allianceauth'"
+sudo mysql -u root -p$MYSQL_ROOT_PASS -e "CREATE USER 'allianceserver'@'localhost' IDENTIFIED BY 'password'"
 sudo mysql -u root -p$MYSQL_ROOT_PASS -e "GRANT ALL PRIVILEGES ON * . * TO 'allianceauth'@'localhost'";
 sudo mysql -u root -p$MYSQL_ROOT_PASS -e "CREATE USER 'alliancemumble'@'localhost' IDENTIFIED BY 'alliancemumble'"
 sudo mysql -u root -p$MYSQL_ROOT_PASS -e "GRANT ALL PRIVILEGES ON * . * TO 'alliancemumble'@'localhost'";
@@ -74,9 +50,7 @@ sudo mysqladmin -p$MYSQL_ROOT_PASS flush-privileges
 
 sudo apt-get -y install rabbitmq-server
 
-source /opt/allianceVenv/bin/activate
 sudo pip install -r requirements.txt
-deactivate
 
 chmod +x *.sh
 
