ba73c4c4659ca03b5fb2642c6db325f4f142bc9e
Adarnof <adarnof@gmail.com>
2015-11-28 06:28:30 +0000

    Updated startup script to collect static and evolve as needed.

--- vagrant/startup.sh
+++ vagrant/startup.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # TODO route log output to file.
+yes yes | python manage.py collectstatic
 python manage.py syncdb
+yes yes | python manage.py evolve --hint --execute
 
 python manage.py shell < run_alliance_corp_update.py
 
