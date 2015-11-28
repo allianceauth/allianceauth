02dc27deac2b44acdb1f4d7eb6f7b62b454c15c2
Adarnof <adarnof@gmail.com>
2015-11-28 05:08:37 +0000

    Corrected ALLIANCE_NAME context processor to render alliance name if IS_CORP false. Added ALLIANCE_NAME vs CORP_NAME toggle for index page title per IS_CORP value. Closes #85

--- util/context_processors.py
+++ util/context_processors.py
@@ -15,7 +15,7 @@ def alliance_id(request):
     return {'ALLIANCE_ID': settings.ALLIANCE_ID}
 
 def alliance_name(request):
-    return {'ALLIANCE_NAME': settings.CORP_NAME}
+    return {'ALLIANCE_NAME': settings.ALLIANCE_NAME}
 
 def jabber_url(request):
     return {'JABBER_URL': settings.JABBER_URL}
