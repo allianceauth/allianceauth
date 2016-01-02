428668c19af5ec2601cd21f883b484f6bf8d647d
orbitroom <orbitroom13@gmail.com>
2016-01-01 23:15:31 -0600

    Delete start_bg_tasks.sh

--- start_bg_tasks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ${0%/*}
-screen -dm bash -c 'python manage.py celeryd --verbosity=2 --loglevel=DEBUG'
-screen -dm bash -c 'python manage.py celerybeat --verbosity=2 --loglevel=DEBUG'
