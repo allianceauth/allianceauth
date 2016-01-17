7aa3a19ba5462da435d74684c23c5a6c47e7c86d
Mr McClain <orbitroom13@gmail.com>
2016-01-17 01:30:51 -0600

    typos fixed

--- sigtracker/form.py
+++ sigtracker/form.py
@@ -10,7 +10,7 @@ class SignatureForm(forms.Form):
 
     system = forms.CharField(max_length=254, required=True, label='System')
     ident = forms.CharField(max_length=254, required=True, label="ID")
-    sigtype = forms.ChoiceField(choices=sigtype, required=True, label="Signiture Type")
+    sigtype = forms.ChoiceField(choices=sigtype, required=True, label="Signature Type")
     destination = forms.CharField(max_length=254, label="destination", required=True, initial="")
     status = forms.ChoiceField(choices=status, required=True, label="Status")
     notes = forms.CharField(max_length=254, label="Notes", required=False, initial="")
