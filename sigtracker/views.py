194556b56d12f0ff45e2dcf4bd06258de99d5fe9
Mr McClain <orbitroom13@gmail.com>
2016-01-17 22:48:22 -0600

    Added Creator and timestamps

--- sigtracker/views.py
+++ sigtracker/views.py
@@ -1,4 +1,4 @@
-import datetime
+
 
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
@@ -6,6 +6,7 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import permission_required
 from django.contrib.auth.decorators import user_passes_test
+from django.utils import timezone
 
 from util import check_if_user_has_permission
 from authentication.managers import AuthServicesInfoManager
@@ -27,7 +28,7 @@ def sigtracker_util_test(user):
 def sigtracker_view(request):
     logger.debug("sigtracker_view called by user %s" % request.user)
     sigtracker_list = sigtracker.objects.all()
-    render_items = {'sigtracker': sigtracker.objects.all()}
+    render_items = {'sigtracker': sigtracker.objects.all(),}
 
     return render_to_response('registered/signaturemanagement.html', render_items, context_instance=RequestContext(request))
 
@@ -38,8 +39,13 @@ def add_signature_view(request):
     logger.debug("add_signature_view called by user %s" % request.user)
     if request.method == 'POST':
         form = SignatureForm(request.POST)
-        logger.debug("Request type POST contains form valid: %s" % form.is_valid())
+	logger.debug("Request type POST contains form valid: %s" % form.is_valid())
         if form.is_valid():
+            #Get Current Time
+            post_time = timezone.now()
+            # Get character
+            auth_info = AuthServicesInfoManager.get_auth_service_info(request.user)
+            character = EveManager.get_character_by_id(auth_info.main_char_id)
             # handle valid form
             sig = sigtracker()
             sig.ident = form.cleaned_data['ident']
@@ -48,6 +54,8 @@ def add_signature_view(request):
             sig.sigtype = form.cleaned_data['sigtype']
             sig.status = form.cleaned_data['status']
             sig.notes = form.cleaned_data['notes']
+            sig.create_time = post_time
+	    sig.eve_character = character
             sig.save()
             return HttpResponseRedirect("/sigtracker/")
     else:
