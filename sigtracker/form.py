c48902ecf8fc5c64243a4a7a46482ee7e4b68240
orbitroom <orbitroom13@gmail.com>
2016-02-06 14:45:39 -0500

    Added Types and Status

--- sigtracker/form.py
+++ sigtracker/form.py
@@ -7,10 +7,10 @@ class SignatureForm(forms.Form):
     sigtype = [('Wormhole', 'Wormhole'), ('Combat', 'Combat'), ('Data', 'Data'),
               ('Relic', 'Relic'), ('Gas', 'Gas'), ('Ore', 'Ore')]
     status = [('Open', 'Open'), ('Started', 'Started'), ('Finished', 'Finished'), ('Life cycle has not begun', 'Life cycle has not begun'),
-             ('Probably wont last another day', 'Probably wont last another day'), ('Reaching the end of its natural lifetime', 'Reaching the end of its natural lifetime'),
-             ('Has not yet had its stability significantly disrupted', 'Has not yet had its stability significantly disrupted'),
-             ('Has had its stability reduced, but not to a critical degree yet', 'Has had its stability reduced, but not to a critical degree yet')
-             ('This wormhole has had its stability critically disrupted', 'This wormhole has had its stability critically disrupted')]
+             ('Probably wont last another day', 'Probably wont last another day'), ('End of its natural lifetime', 'End of its natural lifetime'),
+             ('stability not significantly disrupted', 'stability not significantly disrupted'),
+             ('Stability reduced not critical degree yet', 'Stability reduced not critical degree yet'),
+             ('Wormhole stability critically disrupted', 'Wormhole stability critically disrupted')]
 
     system = forms.CharField(max_length=254, required=True, label='System')
     ident = forms.CharField(max_length=254, required=True, label="ID")
