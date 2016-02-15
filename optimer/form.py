f542b225de97c20aade6c3bbb24c602b6f986fe2
orbitroom <orbitroom13@gmail.com>
2016-02-15 17:56:08 -0500

    Added Date Time Picker to Fleet Operations

--- optimer/form.py
+++ optimer/form.py
@@ -8,7 +8,7 @@ class opForm(forms.Form):
     doctrine = forms.CharField(max_length=254, required=True, label='Doctrine')
     system = forms.CharField(max_length=254, required=True, label="System")
     location = forms.CharField(max_length=254, required=True, label="Location")
-    start = forms.DateTimeField(required=True, label="Start Time MM/DD/YYYY HH:MM:SS")    
+    start = forms.DateTimeField(required=True, label="Start Time")    
     duration = forms.CharField(max_length=254, required=True, label="Duration")
     operation_name = forms.CharField(max_length=254, required=True, label="Operation Name")
     fc = forms.CharField(max_length=254, required=True, label="Fleet Commander")
