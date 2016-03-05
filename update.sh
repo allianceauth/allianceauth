766f4343aa58dfc6e04d5fdd9f09223c8da93f91
Adarnof <adarnof@gmail.com>
2016-03-05 17:53:41 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-pip install --upgrade -r requirements.txt
+pip install -r requirements.txt
 yes yes | python manage.py syncdb
 yes yes | python manage.py evolve --hint --execute
 yes yes | python manage.py collectstatic
