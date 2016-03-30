75459361dc0c8905c11dae0fb65a4dbbb2467a7c
orbitroom <orbitroom13@gmail.com>
2016-03-30 04:04:14 -0500

    Added IP Board 4

--- services/views.py
+++ services/views.py
@@ -14,6 +14,7 @@ from managers.mumble_manager import MumbleManager
 from managers.ipboard_manager import IPBoardManager
 from managers.teamspeak3_manager import Teamspeak3Manager
 from managers.discord_manager import DiscordManager
+from managers.ips4_manager import Ips4Manager
 from authentication.managers import AuthServicesInfoManager
 from eveonline.managers import EveManager
 from celerytask.tasks import update_jabber_groups
@@ -26,7 +27,6 @@ from forms import JabberBroadcastForm
 from forms import FleetFormatterForm
 from forms import DiscordForm
 from forms import ServicePasswordForm
-from forms import TeamspeakJoinForm
 from util import check_if_user_has_permission
 
 import threading
@@ -340,32 +340,12 @@ def activate_teamspeak3(request):
     if result[0] is not "":
         AuthServicesInfoManager.update_user_teamspeak3_info(result[0], result[1], request.user)
         logger.debug("Updated authserviceinfo for user %s with TS3 credentials. Updating groups." % request.user)
+        update_teamspeak3_groups.delay(request.user.pk)
         logger.info("Succesfully activated TS3 for user %s" % request.user)
-        return HttpResponseRedirect("/verify_teamspeak3/")
+        return HttpResponseRedirect("/services/")
     logger.error("Unsuccessful attempt to activate TS3 for user %s" % request.user)
     return HttpResponseRedirect("/dashboard")
 
-@login_required
-@user_passes_test(service_blue_alliance_test)
-def verify_teamspeak3(request):
-    logger.debug("verify_teamspeak3 called by user %s" % request.user)
-    authinfo = AuthServicesInfoManager.get_auth_service_info(request.user)
-    if not authinfo.teamspeak3_uid:
-        logger.warn("Unable to validate user %s teamspeak: no teamspeak data" % request.user)
-        return HttpResponseRedirect("/services")
-    if request.method == "POST":
-        form = TeamspeakJoinForm(request.POST)
-        if form.is_valid():
-            update_teamspeak3_groups.delay(request.user.pk)
-            logger.debug("Validated user %s joined TS server")
-            return HttpResponseRedirect("/services/")
-    else:
-        form = TeamspeakJoinForm({'username':authinfo.teamspeak3_uid})
-    context = {
-        'form': form,
-        'authinfo': authinfo,
-    }
-    return render_to_response('registered/teamspeakjoin.html', context, context_instance=RequestContext(request))
 
 @login_required
 @user_passes_test(service_blue_alliance_test)
@@ -603,3 +583,80 @@ def set_ipboard_password(request):
     logger.debug("Rendering form for user %s" % request.user)
     context = {'form': form, 'service': 'IPBoard', 'error': error}
     return render_to_response('registered/service_password.html', context, context_instance=RequestContext(request))
+
+@login_required
+@user_passes_test(service_blue_alliance_test)
+def activate_ips4(request):
+    logger.debug("activate_ips4 called by user %s" % request.user)
+    authinfo = AuthServicesInfoManager.get_auth_service_info(request.user)
+    # Valid now we get the main characters
+    character = EveManager.get_character_by_id(authinfo.main_char_id)
+    logger.debug("Adding IPS4 user for user %s with main character %s" % (request.user, character))
+    result = Ips4Manager.add_user(character.character_name, request.user.email)
+    # if empty we failed
+    if result[0] != "":
+        AuthServicesInfoManager.update_user_ips4_info(result[0], result[1], result[2], request.user)
+        logger.debug("Updated authserviceinfo for user %s with IPS4 credentials." % request.user)
+        #update_ips4_groups.delay(request.user.pk)
+        logger.info("Succesfully activated IPS4 for user %s" % request.user)
+        return HttpResponseRedirect("/services/")
+    logger.error("Unsuccesful attempt to activate IPS4 for user %s" % request.user)
+    return HttpResponseRedirect("/dashboard")
+
+@login_required
+@user_passes_test(service_blue_alliance_test)
+def reset_ips4_password(request):
+    logger.debug("reset_ips4_password called by user %s" % request.user)
+    authinfo = AuthServicesInfoManager.get_auth_service_info(request.user)
+    result = Ips4Manager.update_user_password(authinfo.ips4_username)
+    # false we failed
+    if result != "":
+        AuthServicesInfoManager.update_user_ips4_info(authinfo.ips4_username, result, id, request.user)
+        logger.info("Succesfully reset IPS4 password for user %s" % request.user)
+        return HttpResponseRedirect("/services/")
+    logger.error("Unsuccessful attempt to reset IPS4 password for user %s" % request.user)
+    return HttpResponseRedirect("/dashboard")
+
+@login_required
+@user_passes_test(service_blue_alliance_test)
+def set_ips4_password(request):
+    logger.debug("set_ips4_password called by user %s" % request.user)
+    error = None
+    if request.method == 'POST':
+        logger.debug("Received POST request with form.")
+        form = ServicePasswordForm(request.POST)
+        logger.debug("Form is valid: %s" % form.is_valid())
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            logger.debug("Form contains password of length %s" % len(password))
+            authinfo = AuthServicesInfoManager.get_auth_service_info(request.user)
+            result = Ips4Manager.update_custom_password(authinfo.ips4_username, plain_password=password)
+            if result != "":
+                AuthServicesInfoManager.update_user_ips4_info(authinfo.ips4_username, result, authinfo.ips4_id, request.user)
+                logger.info("Succesfully reset IPS4 password for user %s" % request.user)
+                return HttpResponseRedirect("/services/")
+            else:
+                logger.error("Failed to install custom IPS4 password for user %s" % request.user)
+                error = "Failed to install custom password."
+        else:
+            error = "Invalid password provided"
+    else:
+        logger.debug("Request is not type POST - providing empty form.")
+        form = ServicePasswordForm()
+
+    logger.debug("Rendering form for user %s" % request.user)
+    context = {'form': form, 'service': 'IPS4', 'error': error}
+    return render_to_response('registered/service_password.html', context, context_instance=RequestContext(request))
+
+@login_required
+@user_passes_test(service_blue_alliance_test)
+def deactivate_ips4(request):
+    logger.debug("deactivate_ips4 called by user %s" % request.user)
+    authinfo = AuthServicesInfoManager.get_auth_service_info(request.user)
+    result = Ips4Manager.delete_user(authinfo.ips4_id)
+    if result != "":
+        AuthServicesInfoManager.update_user_ips4_info("", "", "", request.user)
+        logger.info("Succesfully deactivated IPS4 for user %s" % request.user)
+        return HttpResponseRedirect("/services/")
+    logger.error("Unsuccesful attempt to deactivate IPS4 for user %s" % request.user)
+    return HttpResponseRedirect("/dashboard")
