321ea9cca3936de6c3477990b24599419d7ad465
Mr McClain <orbitroom13@gmail.com>
2016-04-14 08:03:37 -0500

    Closes #379

--- services/managers/phpbb3_manager.py
+++ services/managers/phpbb3_manager.py
@@ -67,7 +67,7 @@ class Phpbb3Manager:
     @staticmethod
     def __santatize_username(username):
         sanatized = username.replace(" ", "_")
-        sanatized = sanatized.replace("'", "")
+        sanatized = sanatized.replace("'", "_")
         return sanatized.lower()
 
     @staticmethod
