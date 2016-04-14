adde2bb7a87783e857159e5f7c7438d41248d2c8
Mr McClain <orbitroom13@gmail.com>
2016-04-14 08:05:36 -0500

    Addresses #379

--- services/managers/mumble_manager.py
+++ services/managers/mumble_manager.py
@@ -17,7 +17,7 @@ class MumbleManager:
     @staticmethod
     def __santatize_username(username):
         sanatized = username.replace(" ", "_")
-        sanatized = sanatized.replace("'", "")
+        sanatized = sanatized.replace("'", "_")
         return sanatized
 
     @staticmethod
