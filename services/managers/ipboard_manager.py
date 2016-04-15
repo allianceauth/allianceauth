3b8a62adafe9dadca5f5aef45c85e9dda7154a16
Mr McClain <orbitroom13@gmail.com>
2016-04-15 03:55:55 -0500

    Escaped mysql character to prevent errors

--- services/managers/ipboard_manager.py
+++ services/managers/ipboard_manager.py
@@ -15,7 +15,7 @@ class IPBoardManager:
     @staticmethod
     def __santatize_username(username):
         sanatized = username.replace(" ", "_")
-        sanatized = sanatized.replace("'", "_")
+        sanatized = username.replace("'", "\\'")
         return sanatized.lower()
 
     @staticmethod
