470e51d8ecdd14fc6bf00bdb18d86da695ee2764
Adarnof <adarnof@gmail.com>
2016-07-11 16:18:21 +0000

    Extend EveCharacter admin page
    display and allow searching of relevant fields

--- eveonline/admin.py
+++ eveonline/admin.py
@@ -5,7 +5,11 @@ from models import EveApiKeyPair
 from models import EveAllianceInfo
 from models import EveCorporationInfo
 
-admin.site.register(EveCharacter)
 admin.site.register(EveApiKeyPair)
 admin.site.register(EveAllianceInfo)
 admin.site.register(EveCorporationInfo)
+
+@admin.register(EveCharacter)
+class EveCharacterAdmin(admin.ModelAdmin):
+  search_fields = ['character_name', 'corporation_name', 'alliance_name', 'user__username']
+  list_display = ('character_name', 'corporation_name', 'alliance_name', 'user')
