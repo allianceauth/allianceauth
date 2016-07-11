92271226332a8f1f06788845a249391362d99d86
Adarnof <adarnof@gmail.com>
2016-07-11 19:17:14 +0000

    correct admin site registration for old django

--- eveonline/admin.py
+++ eveonline/admin.py
@@ -9,7 +9,9 @@ admin.site.register(EveApiKeyPair)
 admin.site.register(EveAllianceInfo)
 admin.site.register(EveCorporationInfo)
 
-@admin.register(EveCharacter)
+
 class EveCharacterAdmin(admin.ModelAdmin):
   search_fields = ['character_name', 'corporation_name', 'alliance_name', 'user__username']
   list_display = ('character_name', 'corporation_name', 'alliance_name', 'user')
+
+admin.site.register(EveCharacter, EveCharacterAdmin)
