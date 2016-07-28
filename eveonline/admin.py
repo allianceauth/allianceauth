507634ffdc6cb28a12ca65ad693b5cf4791cc02e
Adarnof <adarnof@gmail.com>
2016-07-28 12:02:39 -0400

    Add main character field to character admin

--- eveonline/admin.py
+++ eveonline/admin.py
@@ -4,14 +4,27 @@ from models import EveCharacter
 from models import EveApiKeyPair
 from models import EveAllianceInfo
 from models import EveCorporationInfo
+from authentication.managers import AuthServicesInfoManager
 
-admin.site.register(EveApiKeyPair)
 admin.site.register(EveAllianceInfo)
 admin.site.register(EveCorporationInfo)
 
+class EveApiKeyPairAdmin(admin.ModelAdmin):
+    search_fields = ['api_id', 'user__username']
+    list_display = ['api_id', 'user']
 
 class EveCharacterAdmin(admin.ModelAdmin):
-  search_fields = ['character_name', 'corporation_name', 'alliance_name', 'user__username']
-  list_display = ('character_name', 'corporation_name', 'alliance_name', 'user')
+    search_fields = ['character_name', 'corporation_name', 'alliance_name', 'user__username', 'api_id']
+    list_display = ('character_name', 'corporation_name', 'alliance_name', 'user', 'main_character')
+
+    def main_character(self, obj):
+        auth = AuthServicesInfoManager.get_auth_for_user(obj.user)
+        if auth and auth.main_char_id:
+            try:
+                return EveCharacter.objects.get(character_id=auth.main_char_id)
+            except EveCharacter.DoesNotExist:
+                pass
+        return None
 
 admin.site.register(EveCharacter, EveCharacterAdmin)
+admin.site.register(EveApiKeyPair, EveApiKeyPairAdmin)
