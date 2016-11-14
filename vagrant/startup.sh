666490c1692c1932b29c462d316d0bd7a1ae4f2a
Adarnof <adarnof@gmail.com>
2016-11-13 19:34:52 -0500

    correct celery start commands

--- vagrant/startup.sh
+++ vagrant/startup.sh
@@ -7,7 +7,7 @@ yes yes | python manage.py evolve --hint --execute
 
 python manage.py shell < run_alliance_corp_update.py
 
-python manage.py celeryd --verbosity=2 --loglevel=DEBUG &
-python manage.py celerybeat --verbosity=2 --loglevel=DEBUG &
+python manage.py celeryd &
+python manage.py celerybeat &
 
 python manage.py runserver &
