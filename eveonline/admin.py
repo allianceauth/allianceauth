ff3bb9743f3628f19f9add4ba55821233f197a10
Adarnof <adarnof@gmail.com>
2017-01-18 19:52:04 -0500

    Enforce unique IDs and names for eveonline models (#657)
    Should help narrow down #656. Please report IntegrityErrors.

--- eveonline/admin.py
+++ eveonline/admin.py
@@ -13,7 +13,17 @@ admin.site.register(EveCorporationInfo)
 
 class EveApiKeyPairAdmin(admin.ModelAdmin):
     search_fields = ['api_id', 'user__username']
-    list_display = ['api_id', 'user']
+    list_display = ['api_id', 'user', 'characters']
+
+    @staticmethod
+    def characters(obj):
+        return ', '.join(sorted([c.character_name for c in EveCharacter.objects.filter(api_id=obj.api_id)]))
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super(EveApiKeyPairAdmin, self).get_search_results(request, queryset, search_term)
+        chars = EveCharacter.objects.filter(character_name__icontains=search_term)
+        queryset |= EveApiKeyPair.objects.filter(api_id__in=[char.api_id for char in chars if bool(char.api_id)])
+        return queryset, use_distinct
 
 
 class EveCharacterAdmin(admin.ModelAdmin):
--- eveonline/models.py
+++ eveonline/models.py
@@ -6,8 +6,8 @@ from django.contrib.auth.models import User
 
 @python_2_unicode_compatible
 class EveCharacter(models.Model):
-    character_id = models.CharField(max_length=254)
-    character_name = models.CharField(max_length=254)
+    character_id = models.CharField(max_length=254, unique=True)
+    character_name = models.CharField(max_length=254, unique=True)
     corporation_id = models.CharField(max_length=254)
     corporation_name = models.CharField(max_length=254)
     corporation_ticker = models.CharField(max_length=254)
@@ -22,7 +22,7 @@ class EveCharacter(models.Model):
 
 @python_2_unicode_compatible
 class EveApiKeyPair(models.Model):
-    api_id = models.CharField(max_length=254)
+    api_id = models.CharField(max_length=254, unique=True)
     api_key = models.CharField(max_length=254)
     user = models.ForeignKey(User, blank=True, null=True)
     sso_verified = models.BooleanField(default=False)
@@ -33,8 +33,8 @@ class EveApiKeyPair(models.Model):
 
 @python_2_unicode_compatible
 class EveAllianceInfo(models.Model):
-    alliance_id = models.CharField(max_length=254)
-    alliance_name = models.CharField(max_length=254)
+    alliance_id = models.CharField(max_length=254, unique=True)
+    alliance_name = models.CharField(max_length=254, unique=True)
     alliance_ticker = models.CharField(max_length=254)
     executor_corp_id = models.CharField(max_length=254)
     is_blue = models.BooleanField(default=False)
@@ -45,8 +45,8 @@ class EveAllianceInfo(models.Model):
 
 @python_2_unicode_compatible
 class EveCorporationInfo(models.Model):
-    corporation_id = models.CharField(max_length=254)
-    corporation_name = models.CharField(max_length=254)
+    corporation_id = models.CharField(max_length=254, unique=True)
+    corporation_name = models.CharField(max_length=254, unique=True)
     corporation_ticker = models.CharField(max_length=254)
     member_count = models.IntegerField()
     is_blue = models.BooleanField(default=False)
