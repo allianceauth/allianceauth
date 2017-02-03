0902e250d360479bbe1c105c7224158bb1434b81
nizzan <martinmorling@gmail.com>
2017-02-03 12:08:52 +0700

    Update pip packages during auth update (#681)

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
-pip install -r requirements.txt
+pip install --upgrade -r requirements.txt
 python manage.py migrate
 yes yes | python manage.py collectstatic
 python manage.py shell < run_alliance_corp_update.py
