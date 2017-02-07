ff1c2030cafa8bbbcbb3b4f6c0e18a939e0ad6f5
Basraah <basraah@users.noreply.github.com>
2017-02-07 19:04:31 +1000

    Update optimer and fleetup javascript (#695)
    * Split out moment duration formatter
    
    * Replace fleetup timer js
    
    * Replace optimer js
    
    * Remove obsolete countdown.js library
    
    * Remove unused dateformat.js references
    
    * Remove obsolete dateformat library

---
+++ stock/static/js/timers.js
@@ -0,0 +1,15 @@
+/**
+* Get a duration string like countdown.js
+* e.g. "1y 2d 3h 4m 5s"
+* @param duration moment.duration
+*/
+function getDurationString(duration) {
+    var out = "";
+    if (duration.years()) {
+        out += duration.years() + 'y ';
+    }
+    if (duration.days()) {
+        out += duration.days() + 'd ';
+    }
+    return out + duration.hours() + "h " + duration.minutes() + "m " + duration.seconds() + "s";
+}
