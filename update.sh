fe9da4d5de9f8748bdcc7be8386a00d2060cf35e
Basraah <basraah@users.noreply.github.com>
2017-02-12 17:23:33 +1000

    Add fake initial
    Will cause Django to fake any initial migrations for tables that already exist. Required as we have moved some tables to other apps.
    
    If the tables don't exist, the initial migrations run as normal.

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 pip install --upgrade -r requirements.txt
-python manage.py migrate
+python manage.py migrate --fake-initial
 yes yes | python manage.py collectstatic
 python manage.py shell < run_alliance_corp_update.py
