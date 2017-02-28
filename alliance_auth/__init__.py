de47e9487049f6ded4d6cd48e5682e8066519aca
Basraah <basraah@users.noreply.github.com>
2017-02-28 11:39:25 +1000

    Version bump to 1.15.1

--- alliance_auth/__init__.py
+++ alliance_auth/__init__.py
@@ -4,5 +4,5 @@ from __future__ import absolute_import, unicode_literals
 # Django starts so that shared_task will use this app.
 from .celeryapp import app as celery_app  # noqa
 
-__version__ = '1.15.0'
+__version__ = '1.15.1'
 NAME = 'Alliance Auth v%s' % __version__
