aec013b93cc52b5d6b19779c85c8ffcb9fe7b9e8
Basraah <basraah@users.noreply.github.com>
2017-05-05 00:31:57 +1000

    SeAT service cleanup (#796)
    Manager function tidyup
    Hopefully improved the key sync function, at least it should be easier to follow whats happening now.
    Remove partial logging of unhashed passwords
    Added user feedback

--- services/modules/seat/views.py
+++ services/modules/seat/views.py
@@ -1,6 +1,8 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
 
 from .manager import SeatManager
 
@@ -15,6 +17,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 ACCESS_PERM = 'seat.access_seat'
+SERVICE_NAME = {'service': 'SeAT'}
 
 
 @login_required
@@ -39,6 +42,8 @@ def activate_seat(request):
         logger.debug("Updated SeatUser for user %s with SeAT credentials. Adding eve-apis..." % request.user)
         SeatTasks.update_roles.delay(request.user.pk)
         logger.info("Successfully activated SeAT for user %s" % request.user)
+        messages.add_message(request, messages.SUCCESS, _('Successfully activated your %(service)s account.') %
+                             SERVICE_NAME)
         SeatManager.synchronize_eveapis(request.user)
         credentials = {
             'username': request.user.seat.username,
@@ -46,6 +51,9 @@ def activate_seat(request):
         }
         return render(request, 'registered/service_credentials.html',
                       context={'credentials': credentials, 'service': 'SeAT'})
+    messages.add_message(request, messages.ERROR,
+                         _('Failed to activate your %(service)s account, please contact your administrator.') %
+                         SERVICE_NAME)
     logger.error("Unsuccessful attempt to activate seat for user %s" % request.user)
     return redirect("auth_services")
 
@@ -56,10 +64,15 @@ def deactivate_seat(request):
     logger.debug("deactivate_seat called by user %s" % request.user)
     # false we failed
     if SeatTasks.delete_user(request.user):
+        messages.add_message(request, messages.SUCCESS,
+                             _('Successfully deactivated your %(service)s account.') % SERVICE_NAME)
         logger.info("Successfully deactivated SeAT for user %s" % request.user)
         return redirect("auth_services")
     else:
         logging.error("User does not have a SeAT account")
+    messages.add_message(request, messages.ERROR,
+                         _('Failed to deactivate your %(service)s account, please contact your administrator.') %
+                         SERVICE_NAME)
     logger.error("Unsuccessful attempt to activate SeAT for user %s" % request.user)
     return redirect("auth_services")
 
@@ -76,10 +89,15 @@ def reset_seat_password(request):
                 'username': request.user.seat.username,
                 'password': result,
             }
+            messages.add_message(request, messages.SUCCESS,
+                                 _('Successfully reset your %(service)s password.') % {'service': 'SeAT'})
             logger.info("Succesfully reset SeAT password for user %s" % request.user)
             return render(request, 'registered/service_credentials.html',
                           context={'credentials': credentials, 'service': 'SeAT'})
     logger.error("Unsuccessful attempt to reset SeAT password for user %s" % request.user)
+    messages.add_message(request, messages.ERROR,
+                         _('Failed to reset your %(service)s password, please contact your administrator.') %
+                         {'service': 'SeAT'})
     return redirect("auth_services")
 
 
@@ -98,11 +116,17 @@ def set_seat_password(request):
                                                       request.user.email,
                                                       plain_password=password)
             if result:
+                messages.add_message(request, messages.SUCCESS,
+                                     _('Successfully set your %(service)s password.') % SERVICE_NAME)
                 logger.info("Succesfully reset SeAT password for user %s" % request.user)
                 return redirect("auth_services")
             else:
+                messages.add_message(request, messages.ERROR,
+                                     _('Failed to set your %(service)s password, please contact your administrator.') %
+                                     SERVICE_NAME)
                 logger.error("Failed to install custom SeAT password for user %s" % request.user)
         else:
+            messages.add_message(request, messages.ERROR, _('Invalid password.'))
             logger.error("Invalid SeAT password provided")
     else:
         logger.debug("Request is not type POST - providing empty form.")
