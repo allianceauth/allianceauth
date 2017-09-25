ad266ea2ee66652879b7b6667edda9a4404768f5
Adarnof <adarnof@gmail.com>
2017-09-25 18:36:53 -0400

    Increase tested retry after
    Apparently tests take longer than 200ms to evaluate here.

--- services/modules/discord/tests.py
+++ services/modules/discord/tests.py
@@ -402,7 +402,7 @@ class DiscordManagerTestCase(TestCase):
 
         m.patch(request_url,
                 request_headers=headers,
-                headers={'Retry-After': '200'},
+                headers={'Retry-After': '200000'},
                 status_code=429)
 
         # Act & Assert
@@ -410,7 +410,7 @@ class DiscordManagerTestCase(TestCase):
             try:
                 DiscordOAuthManager.update_groups(user_id, groups, blocking=False)
             except manager.DiscordApiBackoff as bo:
-                self.assertEqual(bo.retry_after, 200, 'Retry-After time must be equal to Retry-After set in header')
+                self.assertEqual(bo.retry_after, 200000, 'Retry-After time must be equal to Retry-After set in header')
                 self.assertFalse(bo.global_ratelimit, 'global_ratelimit must be False')
                 raise bo
 
@@ -437,7 +437,7 @@ class DiscordManagerTestCase(TestCase):
 
         m.patch(request_url,
                 request_headers=headers,
-                headers={'Retry-After': '200', 'X-RateLimit-Global': 'true'},
+                headers={'Retry-After': '200000', 'X-RateLimit-Global': 'true'},
                 status_code=429)
 
         # Act & Assert
@@ -445,7 +445,7 @@ class DiscordManagerTestCase(TestCase):
             try:
                 DiscordOAuthManager.update_groups(user_id, groups, blocking=False)
             except manager.DiscordApiBackoff as bo:
-                self.assertEqual(bo.retry_after, 200, 'Retry-After time must be equal to Retry-After set in header')
+                self.assertEqual(bo.retry_after, 200000, 'Retry-After time must be equal to Retry-After set in header')
                 self.assertTrue(bo.global_ratelimit, 'global_ratelimit must be True')
                 raise bo
 
