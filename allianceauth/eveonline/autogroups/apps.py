38baeba254a7eb8830a89a8698acfcefd817fafa
Adarnof <adarnof@gmail.com>
2017-12-21 15:25:53 -0500

    Load signals with app

--- allianceauth/eveonline/autogroups/apps.py
+++ allianceauth/eveonline/autogroups/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EveAutogroupsConfig(AppConfig):
     name = 'allianceauth.eveonline.autogroups'
     label = 'eve_autogroups'
+
+    def ready(self):
+        import .signals
