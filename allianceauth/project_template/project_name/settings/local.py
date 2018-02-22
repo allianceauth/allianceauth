bd5ea38446bc784143238487e43bd07fc51bc10d
Adarnof <adarnof@gmail.com>
2018-02-21 22:32:23 -0500

    Add a warning against editing base.py Beautify local.py by removing big block comments. Move some settings back to base.py which don't need to be in local.py

--- allianceauth/project_template/project_name/settings/local.py
+++ allianceauth/project_template/project_name/settings/local.py
@@ -1,49 +1,29 @@
+# Every setting in base.py can be overloaded by redefining it here.
 from .base import *
 
-# These are required for Django to function properly
+# These are required for Django to function properly. Don't touch.
 ROOT_URLCONF = '{{ project_name }}.urls'
 WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
-STATICFILES_DIRS = [
-    os.path.join(PROJECT_DIR, 'static'),
-]
-STATIC_ROOT = "/var/www/{{ project_name }}/static/"
-TEMPLATES[0]['DIRS'] += [os.path.join(PROJECT_DIR, 'templates')]
 SECRET_KEY = '{{ secret_key }}'
 
-#######################################
-#              Site Name              #
-#######################################
-# Change this to change the name of the
-# auth site displayed in page titles
-# and the site header.
-#######################################
+# This is where css/images will be placed for your webserver to read
+STATIC_ROOT = "/var/www/{{ project_name }}/static/"
+
+# Change this to change the name of the auth site displayed
+# in page titles and the site header.
 SITE_NAME = '{{ project_name }}'
 
-#######################################
-#              Debug Mode             #
-#######################################
-# Change this to enable/disable debug 
-# mode, which displays useful error
-# messages but can leak sensitive data.
-#######################################
+# Change this to enable/disable debug mode, which displays
+# useful error messages but can leak sensitive data.
 DEBUG = False
 
-#######################################
-#       Additional Applications       #
-#######################################
 # Add any additional apps to this list.
-#######################################
 INSTALLED_APPS += [
     
 ]
 
-#######################################
-#         Database Settings           #
-#######################################
-# Uncomment and change the database name
-# and credentials to use MySQL/MariaDB.
-# Leave commented to use sqlite3
-#######################################
+# Uncomment by removing the """ and change the database name and
+# credentials to use MySQL/MariaDB. Leave commented to use sqlite3
 """
 DATABASES['default'] = {
     'ENGINE': 'django.db.backends.mysql',
@@ -55,32 +35,18 @@ DATABASES['default'] = {
 }
 """
 
-#######################################
-#             SSO Settings            #
-#######################################
-# Register an application at
-# https://developers.eveonline.com
-# and fill out these settings.
-# Be sure to set the callback URL to
-# https://example.com/sso/callback
-# substituting your domain for example.com
-# Select all available esi- scopes.
-#######################################
+# Register an application at https://developers.eveonline.com
+# and fill out these settings. Be sure to set the callback URL to
+# https://example.com/sso/callback substituting your domain for example.com
+# Select all available scopes starting with esi-
 ESI_SSO_CLIENT_ID = ''
 ESI_SSO_CLIENT_SECRET = ''
 ESI_SSO_CALLBACK_URL = ''
 
-#######################################
-#            Email Settings           #
-#######################################
-# Alliance Auth validates emails before
-# new users can log in.
-# It's recommended to use a free service
-# like SparkPost or Mailgun to send email.
+# Emails are validated before new users can log in.
+# It's recommended to use a free service like SparkPost or Mailgun to send email.
 # https://www.sparkpost.com/docs/integrations/django/
-# Set the default from email to something
-# like 'noreply@example.com'
-#######################################
+# Set the default from email to something like 'noreply@example.com'
 EMAIL_HOST = ''
 EMAIL_PORT = 587
 EMAIL_HOST_USER = ''
