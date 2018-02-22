d0aa46db089f01966ff61fe1ac96da0deafe5b89
Adarnof <adarnof@gmail.com>
2018-02-22 18:10:24 -0500

    Accept any ordering of groups in test.
    I have no idea why the order is reversing itself. Doesn't matter for functionality which of them comes first. This just checks they're both in there and a comma to separate them.

--- allianceauth/services/modules/mumble/tests.py
+++ allianceauth/services/modules/mumble/tests.py
@@ -136,7 +136,9 @@ class MumbleViewsTestCase(TestCase):
         mumble_user = MumbleUser.objects.get(user=self.member)
         self.assertEqual(mumble_user.username, expected_username)
         self.assertTrue(mumble_user.pwhash)
-        self.assertEqual('Guest,Member', mumble_user.groups)
+        self.assertIn('Guest', mumble_user.groups)
+        self.assertIn('Member', mumble_user.groups)
+        self.assertIn(',', mumble_user.groups)
 
     def test_deactivate_post(self):
         self.login()
