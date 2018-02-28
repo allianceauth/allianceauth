f9a8ac4e9ba18eaaee79cc08a3e14e538616e43c
Adarnof <adarnof@gmail.com>
2018-02-28 13:20:36 -0500

    Bump version to b3 in anticipation of next release.

--- allianceauth/__init__.py
+++ allianceauth/__init__.py
@@ -1,7 +1,7 @@
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
 
-__version__ = '2.0b2'
+__version__ = '2.0b3'
 NAME = 'Alliance Auth v%s' % __version__
 default_app_config = 'allianceauth.apps.AllianceAuthConfig'
 
